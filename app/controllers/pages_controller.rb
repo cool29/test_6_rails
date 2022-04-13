0c78221e958823741da9967241d45bcd84c45d0f
Hamza Ali <erum.khalil@yahoo.com>
2022-04-13 16:37:52 -0400

    Added an about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home # excpects home.html.erb
   end
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home' # Controller from pages action: home
+  get 'about', to: 'pages#about'  # get about page / page
 end
